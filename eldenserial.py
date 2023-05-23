a4343c7945dde24ebfa2cf911b1d24524c7cae93
BambiTango <93163996+Siddh1rt1@users.noreply.github.com>
2023-05-23 10:41:57 +0200

    Add files via upload

---
+++ eldenserial.py
@@ -0,0 +1,55 @@
+from PIL import ImageGrab, Image
+import numpy
+import time
+#img = Image.open('test.png')
+import pyttsx3
+from random import randrange
+engine = pyttsx3.init()
+import serial
+
+# Serielle Verbindung herstellen
+ser = serial.Serial('COM14', 9600)  # COM-Port und Baudrate entsprechend anpassen
+letter = 'A' 
+
+
+ctest=[81,13,12]
+cpe=20
+time.sleep(10)
+left = 50
+top = 52
+right = 800
+bottom = 53
+lengthofarray=right-left
+beleidigung=["du stinkst","du looser","failed","Du bist eine enttäuschung","Ich wünschte du wärest nicht so schlecht","Du legostein ohne noppen","du Apfelspieß ohne apfel, sondern nur mit scheiße","ich glaube du bist einfach nur schlecht","Worst gamer ever","wow, du kannst echt gar nichts","Wenn du fickst, wie du spielst, dann fickst du nicht gut","OH MEIN GOTT; du bist einfach nur erbärmlich","Wenn Jesus dich sehen würde, würde er Gott um die Kreuzigug bitten."]
+timeticker=0
+lastvar=0
+while True:
+    print(timeticker)
+    counter=0
+    timeticker=timeticker+1
+    img=ImageGrab.grab()
+    width, height = img.size
+    cropped = img.crop((left, top, right, bottom))
+    #cropped.show()
+    cropped.convert('RGB')
+    x=0
+    for x in range(0,lengthofarray,5):
+        #print(x)
+        r, g, b = cropped.getpixel((x,0))
+        #print(r,g,b)
+        #print(ctest)
+        if ((ctest[0]-cpe)<=r<=(ctest[0]+cpe)) and ((ctest[1]-cpe)<=g<=(ctest[1]+cpe)) and ((ctest[2]-cpe)<=b<=(ctest[2]+cpe)) :
+            
+            counter=counter+1
+    print(lastvar,counter)
+    if lastvar >counter+5:
+        print("OH NOO")
+        ser.write(letter.encode())
+        #text=beleidigung[randrange(len(beleidigung))]
+        #engine.say(text)
+        #engine.runAndWait()
+    lastvar=counter
+    print("redpixel: "+str(counter))
+    time.sleep(0.5)
+#r, g, b = cropped.getpixel((20,0))
+#print(r,g,b)
